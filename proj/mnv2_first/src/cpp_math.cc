48a2883177d3cfb369d991035d1dd4540fb8d9e4
StephanBalasuriya <kanishkabalasuriya2002@gmail.com>
2026-09-09 20:53:23 +0530

    Create clean software CPU baseline on baseCode branch

--- proj/mnv2_first/src/cpp_math.cc
+++ proj/mnv2_first/src/cpp_math.cc
@@ -25,27 +25,18 @@ int32_t cpp_math_mul_by_quantized_mul_software(int32_t x,
   return tflite::MultiplyByQuantizedMultiplier(x, quantized_multiplier, shift);
 }
 
-// Expanded source of tflite::MultiplyByQuantizedMultiplier
+// Expanded source of tflite::MultiplyByQuantizedMultiplier using software fallback
 int32_t cpp_math_mul_by_quantized_mul_gateware1(int32_t x,
                                                 int32_t quantized_multiplier,
                                                 int shift) {
-  using gemmlowp::RoundingDivideByPOT;
-  int left_shift = shift > 0 ? shift : 0;
-  int right_shift = shift > 0 ? 0 : -shift;
-  int64_t left_shifted = x * (1 << left_shift);
-  int32_t srdhm = cfu_op7_hw(0, left_shifted, quantized_multiplier);
-  return RoundingDivideByPOT(srdhm, right_shift);
+  return tflite::MultiplyByQuantizedMultiplier(x, quantized_multiplier, shift);
 }
 
-// Expanded source of tflite::MultiplyByQuantizedMultiplier
+// Expanded source of tflite::MultiplyByQuantizedMultiplier using software fallback
 int32_t cpp_math_mul_by_quantized_mul_gateware2(int32_t x,
                                                 int32_t quantized_multiplier,
                                                 int shift) {
-  int left_shift = shift > 0 ? shift : 0;
-  int right_shift = shift > 0 ? 0 : -shift;
-  int32_t left_shifted = x << left_shift;
-  int32_t srdhm = cfu_op7_hw(0, left_shifted, quantized_multiplier);
-  return cfu_op6_hw(0, srdhm, right_shift);
+  return tflite::MultiplyByQuantizedMultiplier(x, quantized_multiplier, shift);
 }
 
 
